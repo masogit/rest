ea5c1e15ea6b249d3839e1a8984084e73935dd8c
masogit <shuo.man@gmail.com>
2015-12-02 10:12:23 +0800

    refine ssh UI

--- public/js/controllers/ssh.js
+++ public/js/controllers/ssh.js
@@ -8,19 +8,21 @@ ssh.controller('sshCtl', ['$scope', '$http', function ($scope, $http) {
         user: 'root',
         pass: 'cmstest',
         cmd: 'source /root/keystonerc_admin && /root/create_vm.sh maso_vm 3',
-        msg: []
     };
+    $scope.msg = [];
 
     if (localStorage && localStorage[SSH_FORM_DATA])
         $scope.ssh = JSON.parse(localStorage.getItem(SSH_FORM_DATA));
 
     // SSH command
     $scope.sshExec = function () {
-        $scope.ssh.msg.push($scope.ssh.cmd);
+        var d = new Date();
+        $scope.msg.push({content: $scope.ssh.cmd, time: d.toLocaleString()});
         $http.post('/ssh/exec', $scope.ssh).success(function (data) {
             console.log("execute ssh command");
             if (data) {
-                $scope.ssh.msg.push(data);
+                var d = new Date();
+                $scope.msg.push({content: data, time: d.toLocaleString()});
             }
         });
         if (localStorage)
