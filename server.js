f1e06dabd63b9b8a35bdfd58ef57a53b97a86fc1
masogit <shuo.man@gmail.com>
2016-01-13 14:22:52 +0800

    export module for creating db folder and files

--- server.js
+++ server.js
@@ -5,46 +5,9 @@ var port = process.env.PORT || 8080; 				// set the port
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-const fs = require('fs');
-// var SSH 	= require('simple-ssh');
 
-// check db folder and files
-fs.exists('db', function (db) {
-    if (!db) {
-        console.log("not found db folder");
-
-        fs.mkdir('db', function (err) {
-            if (err) {
-                return console.log(err);
-            }
-
-            console.log("The db folder was created!");
-        })
-
-    }
-
-    fs.exists('db/template.json', function (template) {
-        if (!template)
-            fs.writeFile("db/template.json", "", function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-
-                console.log("The template file was created!");
-            });
-    });
-    fs.exists('db/metadata.json', function (metadata) {
-        if (!metadata)
-            fs.writeFile("db/metadata.json", "", function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-
-                console.log("The metadata file was created!");
-            });
-    });
-
-});
+// initial db folder and files =================================================
+require('./app/db.js')('db');
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
