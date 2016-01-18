c7e89fc38030ad34e7b557a925636249c98f23a5
masogit <shuo.man@gmail.com>
2016-01-18 18:13:58 +0800

    merge mobile to web

--- public/js/controllers/amx.js
+++ public/js/controllers/amx.js
@@ -1,4 +1,16 @@
-var am = angular.module('am', ['ui.bootstrap']);
+var am = angular.module('am', ['ui.bootstrap', 'ngRoute', 'mobile-angular-ui', 'mobile-angular-ui.gestures']);
+
+am.run(function ($transform) {
+    window.$transform = $transform;
+});
+
+am.config(function ($routeProvider) {
+    $routeProvider.when('/m/temp_edit', { templateUrl: '/mobile/temp_edit.html', reloadOnSearch: false });
+    $routeProvider.when('/m/temp_records', { templateUrl: '/mobile/temp_records.html', reloadOnSearch: false });
+    $routeProvider.when('/m/temp_record', { templateUrl: '/mobile/temp_record.html', reloadOnSearch: false });
+    $routeProvider.when('/m/tab_tree', { templateUrl: '/mobile/tab_tree.html', reloadOnSearch: false });
+});
+
 am.controller('amCtl', function ($scope, $http, $uibModal) {
     var AM_FORM_DATA = "amFormData";
     $scope.title = "AM Browser";
